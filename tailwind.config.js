ca26052fe99517fa96eb9f3d46574b6c88396a4d
anujin626 <anu64310@gmail.com>
2021-05-06 21:33:09 +0800

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        dark: "#0A2551"
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
